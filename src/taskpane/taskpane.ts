fe66542599635cd206397275171639d2c2367c80
Jan Schmitz-Hermes <jan@schmitz-hermes.de>
2023-11-21 09:19:45 +0100

    add ability to store custom initials

--- src/taskpane/taskpane.ts
+++ src/taskpane/taskpane.ts
@@ -7,16 +7,22 @@
 
 Office.onReady((info) => {
   if (info.host === Office.HostType.PowerPoint) {
+    let initials = <HTMLInputElement>document.getElementById("initials")
+    initials.value = localStorage.getItem("initials")
+
     document.getElementById("yellow-sticker").onclick = () =>  insertSticker("yellow");
     document.getElementById("cyan-sticker").onclick = () =>  insertSticker("#00ffff");
+    document.getElementById("save-initials").onclick = () => localStorage.setItem("initials", ((<HTMLInputElement>document.getElementById("initials")).value))
   }
 });
 
+
 export async function insertSticker(color) {
   await PowerPoint.run(async (context) => {
-  
+    
+    const today = new Date()
     const shapes = context.presentation.getSelectedSlides().getItemAt(0).shapes
-    const textbox = shapes.addTextBox("Hello 2!");
+    const textbox = shapes.addTextBox(localStorage.getItem("initials") + ", " + today.toDateString() +  "\n");
     textbox.left = 50;
     textbox.top = 50;
     textbox.height = 50;
