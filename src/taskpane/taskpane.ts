6f0fec45aa8f676cb58fb3f81e643d8db8164c7e
Lucas Rolle <lucas.rolle@l21s.de>
2024-01-23 07:21:12 +0100

    add option to fill white background and to remove it

--- src/taskpane/taskpane.ts
+++ src/taskpane/taskpane.ts
@@ -3,40 +3,62 @@
  * See LICENSE in the project root for license information.
  */
 
-/* global document, Office */
+/* global Office, PowerPoint */
+
+import context = Office.context;
 
 Office.onReady((info) => {
   if (info.host === Office.HostType.PowerPoint) {
-    let initials = <HTMLInputElement>document.getElementById("initials")
-    initials.value = localStorage.getItem("initials")
+    let initials = <HTMLInputElement>document.getElementById("initials");
+    initials.value = localStorage.getItem("initials");
 
-    document.getElementById("yellow-sticker").onclick = () =>  insertSticker("yellow");
-    document.getElementById("cyan-sticker").onclick = () =>  insertSticker("#00ffff");
-    document.getElementById("save-initials").onclick = () => localStorage.setItem("initials", ((<HTMLInputElement>document.getElementById("initials")).value))
+    document.getElementById("fill-background").onclick = () => addBackground();
+    document.getElementById("remove-background").onclick = () => removeBackground();
+    document.getElementById("yellow-sticker").onclick = () => insertSticker("yellow");
+    document.getElementById("cyan-sticker").onclick = () => insertSticker("#00ffff");
+    document.getElementById("save-initials").onclick = () =>
+      localStorage.setItem("initials", (<HTMLInputElement>document.getElementById("initials")).value);
   }
 });
 
-
 export async function insertSticker(color) {
-  await PowerPoint.run(async (context) => {
-    
-    const today = new Date()
-    const shapes = context.presentation.getSelectedSlides().getItemAt(0).shapes
-    const textbox = shapes.addTextBox(localStorage.getItem("initials") + ", " + today.toDateString() +  "\n");
+  await runPowerPoint((powerPointContext) => {
+    const today = new Date();
+    const shapes = powerPointContext.presentation.getSelectedSlides().getItemAt(0).shapes;
+    const textbox = shapes.addTextBox(localStorage.getItem("initials") + ", " + today.toDateString() + "\n");
     textbox.left = 50;
     textbox.top = 50;
     textbox.height = 50;
     textbox.width = 150;
     textbox.name = "Square";
-    textbox.fill.setSolidColor(color)
-    textbox.textFrame.textRange.font.bold = true
-    textbox.textFrame.textRange.font.name = "Arial"
-    textbox.textFrame.textRange.font.size = 12
-    textbox.textFrame.textRange.font.color = "#5A5A5A"
-    textbox.lineFormat.visible = true
-    textbox.lineFormat.color = "#000000"
-    textbox.lineFormat.weight = 1.5
-    console.log(textbox.lineFormat.toJSON)
+    textbox.fill.setSolidColor(color);
+    textbox.textFrame.textRange.font.bold = true;
+    textbox.textFrame.textRange.font.name = "Arial";
+    textbox.textFrame.textRange.font.size = 12;
+    textbox.textFrame.textRange.font.color = "#5A5A5A";
+    textbox.lineFormat.visible = false;
+    textbox.lineFormat.visible = false;
+    console.log(textbox.lineFormat.toJSON);
+  });
+}
+
+export async function addBackground() {
+  await runPowerPoint((powerPointContext) => {
+    const selectedImage = powerPointContext.presentation.getSelectedShapes().getItemAt(0);
+    selectedImage.fill.setSolidColor("white");
+  });
+}
+
+export async function removeBackground() {
+  await runPowerPoint((powerPointContext) => {
+    const selectedImage = powerPointContext.presentation.getSelectedShapes().getItemAt(0);
+    selectedImage.fill.clear();
+  });
+}
+
+export async function runPowerPoint(updateFunction: (context: PowerPoint.RequestContext) => void) {
+  await PowerPoint.run(async (context) => {
+    updateFunction(context);
     await context.sync();
   });
 }
