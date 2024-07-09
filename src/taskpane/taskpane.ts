39b3b8f1b087a44565d38a83827925b1a079836d
fabiankuenzer <fabiankuenzer@gmail.com>
2024-07-09 10:49:32 +0200

    Add filter parameters to freepik api request

--- src/taskpane/taskpane.ts
+++ src/taskpane/taskpane.ts
@@ -148,7 +148,7 @@ export async function addBackground(backgroundColor?: string) {
 }
 
 export async function fetchIcons(searchTerm: string): Promise<Array<string>> {
-  const url = "https://hammerhead-app-fj5ps.ondigitalocean.app/icons?term=" + searchTerm;
+  const url = `https://hammerhead-app-fj5ps.ondigitalocean.app/icons?term=${searchTerm}&family-id=300&filters[shape]=outline&filters[color]=solid-black&filters[free_svg]=premium`;
   const requestHeaders = new Headers();
   requestHeaders.append("X-Freepik-API-Key", "FPSX6fb1f23cbea7497387b5e5b8eb8943de");
   const requestOptions = {
