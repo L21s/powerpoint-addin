ce3764aaf18acbd174a236d99b778102849c0348
fabiankuenzer <fabiankuenzer@gmail.com>
2024-07-12 10:31:02 +0200

    Add function to use encrypted api key

--- src/taskpane/encryptionUtils.ts
+++ src/taskpane/encryptionUtils.ts
@@ -1,7 +1,17 @@
+import CryptoJS from "crypto-js";
+
+const encryptedFreepikApiKey =
+  "U2FsdGVkX18258oINhW6ItarhxVnw+paVm8IdfMpDwfw8+I+aJeBKEBK6dz/6wFptAVG5SB+N3ljsBaqN9X2yw==";
+
 export function storeFreepikApiKeyEncryptionSecret() {
   document.getElementById("save-api-key-secret").onclick = () => {
     const encryptionSecret = (<HTMLInputElement>document.getElementById("api-key-secret")).value;
     localStorage.setItem("apiKeySecret", encryptionSecret);
     (<HTMLTextAreaElement>document.getElementById("api-key-secret")).value = "API key secret stored";
   };
+}
+
+export function getDecryptedFreepikApiKey(): string {
+  const decryptedBytes = CryptoJS.AES.decrypt(encryptedFreepikApiKey, localStorage.getItem("apiKeySecret"));
+  return decryptedBytes.toString(CryptoJS.enc.Utf8);
 }
