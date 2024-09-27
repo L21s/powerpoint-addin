6caae7662ccb47fb7241426ecb58fbf5d187a166
Fabian Künzer <85928453+fabiankuenzer@users.noreply.github.com>
2024-09-27 09:16:15 +0200

    Feature: Improve encryption key wording (#28)
    * Refactor wording in encryption helper function
    
    * Refactor wording on html layout
    
    * Refactor wording in README

--- src/taskpane/encryptionUtils.ts
+++ src/taskpane/encryptionUtils.ts
@@ -3,15 +3,15 @@ import CryptoJS from "crypto-js";
 const encryptedFreepikApiKey =
   "U2FsdGVkX1+uakX7Pa7rDwZUW41gsxUcc5Mk1Zf9Ff/RkAhy5TT6snpzfSoe7kn+A7ulqhvrLMasq4hJ/KkwoA==";
 
-export function storeFreepikApiKeySecret() {
-  document.getElementById("save-api-key-secret").onclick = () => {
-    const encryptionSecret = (<HTMLInputElement>document.getElementById("api-key-secret")).value;
-    localStorage.setItem("apiKeySecret", encryptionSecret);
-    (<HTMLTextAreaElement>document.getElementById("api-key-secret")).value = "API key secret stored";
+export function storeEncryptionKey() {
+  document.getElementById("save-encryption-key").onclick = () => {
+    const encryptionSecret = (<HTMLInputElement>document.getElementById("encryption-key")).value;
+    localStorage.setItem("encryptionKey", encryptionSecret);
+    (<HTMLTextAreaElement>document.getElementById("encryption-key")).value = "Encryption key saved";
   };
 }
 
 export function getDecryptedFreepikApiKey(): string {
-  const decryptedBytes = CryptoJS.AES.decrypt(encryptedFreepikApiKey, localStorage.getItem("apiKeySecret"));
+  const decryptedBytes = CryptoJS.AES.decrypt(encryptedFreepikApiKey, localStorage.getItem("encryptionKey"));
   return decryptedBytes.toString(CryptoJS.enc.Utf8);
 }
