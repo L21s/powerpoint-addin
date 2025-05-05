4050d845c536bc11b4df535cfe39a8cb3f8850ed
janeichhorn <jan.eichhorn@l21s.de>
2025-05-05 18:07:40 +0200

    [feature/azure_client_auth_flow] - reworked azure login flow with MSAL Library;

---
+++ src/security/authService.ts
@@ -0,0 +1,45 @@
+import { PublicClientApplication } from "@azure/msal-browser";
+
+const clientId = "236e86e4-f190-48f7-be93-e794ed28382a";
+const redirectUri = "https://localhost:3000/auth-callback.html";
+const tenantId = "a40cd802-97bc-4645-88f7-89bff678a616";
+
+const msalConfig = {
+    auth: {
+        clientId: clientId,
+        authority: `https://login.microsoftonline.com/a40cd802-97bc-4645-88f7-89bff678a616/v2.0`,
+        redirectUri: redirectUri,
+        supportsNestedAppAuth: true,
+        tenantId: tenantId
+    },
+    cache: {
+        cacheLocation: 'sessionStorage',
+        storeAuthStateInCookie: false, //?
+    },
+};
+
+const loginRequest = {
+    scopes: ["profile openid api://b4a241d6-4efe-497e-af56-8fc3a236f4d2/Freepik"]
+};
+
+const msalApp = new PublicClientApplication(msalConfig);
+msalApp.initialize();
+
+export async function loginWithDialog() {
+
+    await msalApp.loginPopup(loginRequest).then(loginResponse => {msalApp.setActiveAccount(loginResponse.account);})
+    console.log(msalApp.getActiveAccount());
+    getToken();
+}
+
+export function getToken() {
+
+    const activeAccount = msalApp.getActiveAccount();
+    console.log(`active account: `,activeAccount);
+    const tokenRequest = {
+        scopes: ["profile openid api://b4a241d6-4efe-497e-af56-8fc3a236f4d2/Freepik"],
+        account: activeAccount,
+    };
+
+    msalApp.acquireTokenSilent(tokenRequest).then(tokenResponse => {return console.log(tokenResponse.accessToken)})
+}
