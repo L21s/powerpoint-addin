c4ed79b9981ed81d0bb329734e36bdd72fb4fb0d
janeichhorn <jan.eichhorn@l21s.de>
2025-05-08 16:12:03 +0200

    [quickfix] Tried another redirect uri for deployment;

--- src/security/authService.ts
+++ src/security/authService.ts
@@ -1,14 +1,16 @@
-import { PublicClientApplication } from "@azure/msal-browser";
+import { PublicClientApplication, createNestablePublicClientApplication, IPublicClientApplication} from "@azure/msal-browser";
 
 const clientId = "236e86e4-f190-48f7-be93-e794ed28382a";
 const authUri = `https://login.microsoftonline.com/a40cd802-97bc-4645-88f7-89bff678a616/v2.0`;
 const scopes = "profile openid api://b4a241d6-4efe-497e-af56-8fc3a236f4d2/Freepik";
+const redirectUri = `https://l21s.github.io/powerpoint-addin/taskpane.html`;
 
 const msalConfig = {
     auth: {
         clientId: clientId,
         authority: authUri,
-        supportsNestedAppAuth: true
+        supportsNestedAppAuth: true,
+        redirectUri: redirectUri,
     },
     cache: {
         cacheLocation: 'sessionStorage',
