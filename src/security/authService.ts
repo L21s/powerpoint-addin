f8d239b0bb6c0d6b42d846613d0d2236d60cafd4
janeichhorn <jan.eichhorn@l21s.de>
2025-05-08 16:24:28 +0200

    [quickfix] Tried another redirect uri for deployment;

--- src/security/authService.ts
+++ src/security/authService.ts
@@ -3,7 +3,7 @@ import { PublicClientApplication, createNestablePublicClientApplication, IPublic
 const clientId = "236e86e4-f190-48f7-be93-e794ed28382a";
 const authUri = `https://login.microsoftonline.com/a40cd802-97bc-4645-88f7-89bff678a616/v2.0`;
 const scopes = "profile openid api://b4a241d6-4efe-497e-af56-8fc3a236f4d2/Freepik";
-const redirectUri = `https://l21s.github.io/powerpoint-addin/taskpane.html`;
+const redirectUri = "https://localhost:3000/auth-callback.html";
 
 const msalConfig = {
     auth: {
