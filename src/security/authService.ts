4d2d9f4c5cc779c000ffae3e863e961a8c40e75a
janeichhorn <jan.eichhorn@l21s.de>
2025-05-08 17:03:22 +0200

    [quickfix] removed callback;

--- src/security/authService.ts
+++ src/security/authService.ts
@@ -1,16 +1,14 @@
-import { PublicClientApplication, createNestablePublicClientApplication, IPublicClientApplication} from "@azure/msal-browser";
+import { PublicClientApplication } from "@azure/msal-browser";
 
 const clientId = "236e86e4-f190-48f7-be93-e794ed28382a";
-const authUri = `https://login.microsoftonline.com/a40cd802-97bc-4645-88f7-89bff678a616/v2.0`;
+const authUri = "https://login.microsoftonline.com/a40cd802-97bc-4645-88f7-89bff678a616/v2.0";
 const scopes = "profile openid api://b4a241d6-4efe-497e-af56-8fc3a236f4d2/Freepik";
-const redirectUri = "https://localhost:3000/auth-callback.html";
 
 const msalConfig = {
     auth: {
         clientId: clientId,
         authority: authUri,
-        supportsNestedAppAuth: true,
-        redirectUri: redirectUri,
+        supportsNestedAppAuth: true
     },
     cache: {
         cacheLocation: 'sessionStorage',
