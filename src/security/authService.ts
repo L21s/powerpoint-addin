e1f8601e50c893fd5203006d52ab6445e1326b95
janeichhorn <jan.eichhorn@l21s.de>
2025-05-08 14:57:46 +0200

    [quickfix] Removed redirect uri attribute from msal config as this causes an error on deployed version; instead msal itself handles this;

--- src/security/authService.ts
+++ src/security/authService.ts
@@ -2,14 +2,12 @@ import { PublicClientApplication } from "@azure/msal-browser";
 
 const clientId = "236e86e4-f190-48f7-be93-e794ed28382a";
 const authUri = `https://login.microsoftonline.com/a40cd802-97bc-4645-88f7-89bff678a616/v2.0`;
-const redirectUri = "https://localhost:3000/taskpane.html";
 const scopes = "profile openid api://b4a241d6-4efe-497e-af56-8fc3a236f4d2/Freepik";
 
 const msalConfig = {
     auth: {
         clientId: clientId,
         authority: authUri,
-        redirectUri: redirectUri,
         supportsNestedAppAuth: true
     },
     cache: {
