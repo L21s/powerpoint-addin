3b3f76d19fb7c5622f3a45ee0ddf524ce8e7ffac
janeichhorn <jan.eichhorn@l21s.de>
2025-05-15 09:39:34 +0200

    [feature/employee_images_api] Removed usage of local storage for images;

--- src/taskpane/employeeApiService.ts
+++ src/taskpane/employeeApiService.ts
@@ -1,9 +1,10 @@
 import {getRequestHeadersWithAuthorization} from "../security/authService";
 
 const proxyBaseUrlEmployees = `https://powerpoint-addin-ktor-pq9vk.ondigitalocean.app/employees`;
-//const proxyBaseUrlEmployeesDEV = `https://localhost:8443/employees`;
+const proxyBaseUrlEmployeesDEV = `https://localhost:8443/employees`;
 let employeeNames: string[] =  [];
 let recentEtagNames: string = "";
+const sharedImageCash: Map<string, string> = new Map<string, string>();
 
 
 async function getRequestOptions(etag?: string): Promise<RequestInit> {
@@ -40,29 +41,20 @@ export async function fetchEmployeeNames() {
 
 export async function fetchEmployeeImage(name: string): Promise<string> {
     try {
-        const result = await fetch(proxyBaseUrlEmployees+`/${name}`, await getRequestOptions(localStorage.getItem(`etag_${name}`)));
+        const result = await fetch(proxyBaseUrlEmployees+`/${name}`, await getRequestOptions(sharedImageCash.get(`etag_${name}`)));
         if (result.status === 304) {
             console.log("Employee image unchanged. Skipping update.");
-            return localStorage.getItem(`image_${name}`);
+            return sharedImageCash.get(`image_${name}`);
         }
         const response: string = await result.text();
-        localStorage.setItem(`image_${name}`, response);
+        sharedImageCash.set(`image_${name}`, response);
         const newEtag = result.headers.get("ETag");
         if (newEtag) {
-            localStorage.setItem(`etag_${name}`, newEtag);
+            sharedImageCash.set(`etag_${name}`, newEtag);
         }
         console.log('fetchImage:', response );
         return response;
     } catch (e) {
-        switch (e.name) {
-            case "QuotaExceededError": {
-                console.warn('Local Storage Limit exceeded: clearing cache.');
-                await fetchEmployeeImage(name);
-                return;
-            }
-            default: {
-                throw new Error("Error fetching employee image: " + e);
-            }
-        }
+        throw new Error("Error fetching employee image: " + e);
     }
 }
