f6887aa20db47882df2dde6ce94827d584b7c824
Tung Tuan Tran <tung.tuan.tran@l21s.de>
2025-07-02 13:05:02 +0200

    refactor stickyNotes textBox

--- src/app/actions/stickyNotes.ts
+++ src/app/actions/stickyNotes.ts
@@ -1,31 +1,37 @@
 import {runPowerPoint} from "../shared/utils/powerPointUtil";
+import ShapeCollection = PowerPoint.ShapeCollection;
 
 export async function insertSticker(color: string) {
   await runPowerPoint((powerPointContext) => {
-    const today = new Date();
     const shapes = powerPointContext.presentation.getSelectedSlides().getItemAt(0).shapes;
-    const textBox = shapes.addTextBox(localStorage.getItem("initials") + ", " + today.toDateString() + "\n", {
-      height: 50,
-      left: 50,
-      top: 50,
-      width: 150,
-    });
-    textBox.name = "Square";
-    textBox.fill.setSolidColor(color);
-    textBox.textFrame.bottomMargin = 7.087; // 0.25 cm in pt
-    textBox.textFrame.topMargin = 7.087;
-    textBox.textFrame.leftMargin = 4.2525; // 0.15 cm in pt
-    textBox.textFrame.rightMargin = 4.2525;
-    setStickerFontProperties(textBox);
+    createTextBox(shapes, color);
   });
 }
 
-function setStickerFontProperties(textbox: PowerPoint.Shape) {
-  textbox.textFrame.textRange.font.bold = true;
-  textbox.textFrame.textRange.font.name = "Arial";
-  textbox.textFrame.textRange.font.size = 12;
-  textbox.textFrame.textRange.font.color = "#5A5A5A";
-  textbox.lineFormat.visible = true;
-  textbox.lineFormat.color = "#000000";
-  textbox.lineFormat.weight = 1.25;
+function createTextBox(shapes: ShapeCollection, color: string) {
+  const today = new Date();
+  const textBox = shapes.addTextBox(localStorage.getItem("initials") + ", " + today.toDateString() + "\n", {
+    height: 50,
+    left: 50,
+    top: 50,
+    width: 150,
+  });
+  textBox.name = "Square";
+  textBox.fill.setSolidColor(color);
+  setTextProperties(textBox);
+  return textBox;
+}
+
+function setTextProperties(textBox: PowerPoint.Shape) {
+  textBox.textFrame.bottomMargin = 7.087; // 0.25 cm in pt
+  textBox.textFrame.topMargin = 7.087;
+  textBox.textFrame.leftMargin = 4.2525; // 0.15 cm in pt
+  textBox.textFrame.rightMargin = 4.2525;
+  textBox.textFrame.textRange.font.bold = true;
+  textBox.textFrame.textRange.font.name = "Arial";
+  textBox.textFrame.textRange.font.size = 12;
+  textBox.textFrame.textRange.font.color = "#5A5A5A";
+  textBox.lineFormat.visible = true;
+  textBox.lineFormat.color = "#000000";
+  textBox.lineFormat.weight = 1.25;
 }
